16470a7ad1b05e8ca23793e4bacc8f7941ffcae8
Ilia Rodionov <ilya.rodionov@gmail.com>
2018-03-12 16:35:06 +0400

    docker global settings

--- src/com/seed/pipeline/GlobalSettings.groovy
+++ src/com/seed/pipeline/GlobalSettings.groovy
@@ -4,5 +4,10 @@ package com.seed.pipeline
  * Глобальные настройки, которые можно использовать во всех проектах.
  */
 class GlobalSettings {
+
+    // Docker registry settings
     public static final String DOCKER_REGISTRY = 'ci3.iseed.pw:5000'
+    public static final String DOCKER_REGISTRY_URL = "http://$DOCKER_REGISTRY"
+    public static final String DOCKER_CREDENTIALS = 'seed.registry'
+
 }
