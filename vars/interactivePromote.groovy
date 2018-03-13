84640e0858e5c469da54dd05e78a6031c912c8fb
Ilia Rodionov <ilya.rodionov@gmail.com>
2018-03-13 11:18:38 +0400

    add interactivePromote

---
+++ vars/interactivePromote.groovy
@@ -0,0 +1,30 @@
+/**
+ * Интерактивный promote
+ */
+def call(promotionTargets, imageTag, version, dockerRegistry) {
+
+    // собираем параметры
+    def params = []
+    promotionTargets.each { targetKey, target ->
+        def item = [:]
+        item.$class = 'BooleanParameterDefinition'
+        item.defaultValue = targetKey != 'dummy'
+        item.name = targetKey
+        item.description = target['description']
+        params.add(item)
+    }
+
+    def targets = [:]
+    timeout(time: 180, unit: 'SECONDS') {
+        targets = input(id: 'Promote', message: 'Let\'s promote to Docker targets?',
+                parameters: params)
+    }
+
+    targets.each { k, v ->
+        if (v)
+            stage(k) {
+                target = promotionTargets[k]
+                deployDockerSimple(imageTag, version, dockerRegistry, v)
+            }
+    }
+}
